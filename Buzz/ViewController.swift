bc179a9d2efdf5f3d2a631986bd5debe0cfedfe7
vhenriquebm <vhenriquebm@gmail.com>
2024-10-18 13:17:21 -0300

    Initial Commit

---
+++ Buzz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Buzz
+//
+//  Created by Vitor Henrique Barreiro Marinho on 18/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
